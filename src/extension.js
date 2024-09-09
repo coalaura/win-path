ed8fb0b3077a131a09344dbaaff9b2eadbe322ba
Laura <milanbartky@gmail.com>
2024-09-09 23:57:06 +0200

    implementation

--- src/extension.js
+++ src/extension.js
@@ -1,3 +1,5 @@
+import vscode from 'vscode';
+
 let activated = false;
 
 /**
@@ -8,7 +10,23 @@ export function activate(context) {
 
 	activated = true;
 
-	// epic
+	vscode.workspace.onDidChangeTextDocument(event => {
+		const edit = event.contentChanges[0],
+			text = edit?.text;
+
+		if (!text || text.length <= 2) return;
+
+		if (!text.match(/^([A-Z]:|\.{1,2})[\w .\\/-]+$/mi)) return;
+
+		const start = edit.range.start,
+			end = start.translate(0, text.length),
+			range = new vscode.Range(start, end);
+
+		const workspaceEdit = new vscode.WorkspaceEdit();
+		workspaceEdit.replace(event.document.uri, range, text.replace(/\\/g, '\\\\'));
+
+		vscode.workspace.applyEdit(workspaceEdit);
+	}, null, context.subscriptions);
 }
 
 export function deactivate() { }
